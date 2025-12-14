6f550adb57d8475e61b01b5f8195e327ca93862b
RANIA <rania.zhirii@gmail.com>
2025-12-15 00:06:34 +0100

    update

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloController {
+
+
     @GetMapping
     public String hello(){
         return "Hello from spring boot application :)";
